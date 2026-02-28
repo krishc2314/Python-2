286bfed6bdbe4543f774d838bcd476c865881171
krishc2314 <krishchitkara2314@gmail.com>
2026-02-28 22:19:51 +0530

    Create shapes.py

---
+++ shapes.py
@@ -0,0 +1,12 @@
+pi=3.14
+def circle(r):
+    circle_area=pi*r*r
+    return circle_area
+
+def rectangle(l,b):
+    rectangle_area=l*b
+    return rectangle_area
+
+def triangle(b,h):
+    triangle_area=0.5*b*h
+    return triangle_area
